ff26a41bac04f52a71bf6a4441bb59822ffbc08a
StragglyRodney <rodney.crudenpowell@gmail.com>
2017-10-05 17:05:12 +1300

    Added basic menu

---
+++ Assets/Main/Scripts/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Menu : MonoBehaviour 
+	{
+		public Canvas MainCanvas;
+		
+
+		void Awake()
+
+		{
+			
+		}
+
+
+		public void OptionsOn()
+	{
+		
+		MainCanvas.enabled = false;
+	}
+
+		public void ReturnOn()
+	{
+		
+		MainCanvas.enabled = true;
+	}
+
+		public void LoadOn()
+		{
+		Application.LoadLevel (1);
+		}
+}
+
+
