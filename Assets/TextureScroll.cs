a978fd4cf89a3b1ca7c8c6a1b38821eb3ab3259f
Fergus Lees <fergus9101@gmail.com>
2017-10-08 21:04:35 +1300

    Made lava texture scroll

---
+++ Assets/TextureScroll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureScroll : MonoBehaviour {
+
+    public float scrollX = 1f;
+    public float scrollY = 1f;
+
+	// Update is called once per frame
+	void Update () {
+        float offsetX = Time.time * scrollX;
+        float offsetY = Time.time * scrollY;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offsetX, offsetY);
+    }
+}
