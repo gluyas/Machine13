d2d58ba5e7e57eab5e412499f98521bc311b00fb
Fergus Lees <leesferg@b-03-05-lab.mddn.vuw.ac.nz>
2017-10-12 16:32:39 +1300

    Fixed nailgun collisions and sound

--- Assets/Scripts/Weapons/ShotGunBulletProperties.cs
+++ Assets/Scripts/Weapons/ShotGunBulletProperties.cs
@@ -6,6 +6,7 @@ namespace Main.Scripts.BulletProperties
 
 		public float Lifetime = 2.0f;           //Time untill bullets clear
 		public int Damage = 1;
+		public int Penetration = 5;
 		int _health;
 	
 		void Start () {
@@ -17,8 +18,16 @@ namespace Main.Scripts.BulletProperties
 			if (other.gameObject.CompareTag("Hitbox"))
 			{
 				other.gameObject.GetComponent<EntityHealth>().Health -= Damage;
-                FindObjectOfType<AudioManager>().Play("hitTarget");
-                Destroy(gameObject);
+				if (other.gameObject.GetComponent<EntityHealth>().Health >= 1)
+					Penetration = 0;
+				//Select Sounds
+				if (other.gameObject.CompareTag("Hitbox") && other.gameObject.layer != 13)
+					FindObjectOfType<AudioManager>().Play("hitTarget");
+				if (other.gameObject.layer == 13)
+					FindObjectOfType<AudioManager>().Play("codHit");
+				if (Penetration <= 0) {
+					Destroy(gameObject);
+				}
 			}
 			if (other.gameObject.CompareTag("Matter") || other.gameObject.CompareTag("Hazard"))
 			{
