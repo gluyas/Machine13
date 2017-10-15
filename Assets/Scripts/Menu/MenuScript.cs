901d972a04dee9160cabcb678dbd2515e25ece57
Rodney Cruden-Powell <rodney.crudenpowell@gmail.com>
2017-10-15 15:58:36 +1300

    implementing menu

---
+++ Assets/Scripts/Menu/MenuScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+
+    public enum MenuStates{Main, Options}
+    public MenuStates CurrentMenuState;
+
+    public GameObject MainMenu;
+    public GameObject OptionsMenu;
+
+    void Awake()
+    {
+        CurrentMenuState = MenuStates.Main; 
+    }
+
+    public void OnPlay()
+    {
+        Debug.Log("Play");  
+    }
+    
+    public void OnOptions()
+    {
+        Debug.Log("Options");
+    }
+    
+    public void OnQuit()
+    {
+        Debug.Log("Quit");
+    }
+
+    public void OnSensitivity()
+    {
+        Debug.Log("Sensitivity");
+    }
+
+    public void OnMainMenu()
+    {
+        Debug.Log("Main Menu");
+    }
+}
